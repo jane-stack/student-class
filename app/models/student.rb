e54dab0b72ca8a02bffb0f2626b746731e616865
Tuyen Ha <98144714+jane-stack@users.noreply.github.com>
2023-06-23 15:36:08 -0600

    validations for student with unique email and password requirements

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,4 +3,35 @@ class Student < ApplicationRecord
 
     has_many :courses, dependent: :destroy
     has_many :registers, through: :courses
+
+    validates :first_name, :last_name, presence: true
+    validates :email, uniqueness: true, presence: true
+    validates :password, length: { minimum: 8 }
+
+    validate :password_lower_case
+    validate :password_uppercase
+    validate :password_special_char
+    validate :password_contains_number
+    
+    def password_uppercase
+        return if !!password.match(/\p{Upper}/)
+        errors.add :password, ' must contain at least 1 uppercase '
+    end
+
+    def password_lower_case
+        return if !!password.match(/\p{Lower}/)
+        errors.add :password, ' must contain at least 1 lowercase '
+    end
+
+    def password_special_char
+        special = "?<>',?[]}{=-)(*&^%$#`~{}!"
+        regex = /[#{special.gsub(/./){|char| "\\#{char}"}}]/
+        return if password =~ regex
+        errors.add :password, ' must contain special character'
+    end
+
+    def password_contains_number
+        return if password.count("0-9") > 0
+        errors.add :password, ' must contain at least one number'
+    end
 end
