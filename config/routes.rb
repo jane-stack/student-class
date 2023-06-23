0378212fae966beed1df1580176f346444035d6e
Tuyen Ha <98144714+jane-stack@users.noreply.github.com>
2023-06-23 17:17:09 -0600

    updated routes for registers and nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
 
-  resources :courses
   resources :students, only: [:index] do
-    resources :registers, only: [:index, :create]
+    resources :registers, only: [:create]
   end
+  resources :courses do
+    resources :registers, only: [:create]
+  end
+  resources :registers, only: [:destroy]
 
   # Students
   post '/signup', to: 'students#create'
@@ -12,6 +15,10 @@ Rails.application.routes.draw do
   # Sessions
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+
+  # Registers
+  get '/courses/:id/add_student', to: 'registers#add_student', as: 'add_student'
+  post '/courses/:id/add_student', to: 'registers#create', as: 'create_registration'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
