867a03c7f2ac2c7003bf709d81ac0d59a8dbd35a
Tuyen Ha <98144714+jane-stack@users.noreply.github.com>
2023-06-23 15:20:57 -0600

    created resources for student with password digest

---
+++ db/migrate/20230623212010_create_students.rb
@@ -0,0 +1,12 @@
+class CreateStudents < ActiveRecord::Migration[7.0]
+  def change
+    create_table :students do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :username
+      t.string :password_digest
+
+      t.timestamps
+    end
+  end
+end
