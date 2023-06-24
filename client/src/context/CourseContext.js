bda2e869e5e526f52f6933aa4318beaf33ccf98f
Tuyen Ha <98144714+jane-stack@users.noreply.github.com>
2023-06-24 14:24:16 -0600

    updated courseContext

--- client/src/context/CourseContext.js
+++ client/src/context/CourseContext.js
@@ -1,15 +1,16 @@
 import { createContext, useEffect, useState } from "react";
 
-const CourseContext = createContext({ children });
-const [ courses, setCourses ] = useState([]);
+const CourseContext = createContext([]);
 
-useEffect(() => {
-    fetch('/courses')
-    .then(resp => resp.json())
-    .then(courses => setCourses(courses))
-}, [])
+const CourseProvider = ({ children }) => {
+    const [ courses, setCourses ] = useState([]);
 
-const CourseProvider = () => {
+    useEffect(() => {
+        fetch('/courses')
+        .then(resp => resp.json())
+        .then(courses => setCourses(courses))
+    }, [])
+    
     return (
         <CourseContext.Provider value={{ courses, setCourses }}>{ children }</CourseContext.Provider>
     )
--- client/src/pages/CourseList.js
+++ client/src/pages/CourseList.js
@@ -1,6 +1,10 @@
+import { useContext } from "react";
+import { CourseContext } from "../context/CourseContext";
 import CourseCard from "../components/CourseCard";
 
 function CourseList() {
+    const { courses, setCourses } = useContext(CourseContext)
+
     return (
         <div><CourseCard /></div>
     )
