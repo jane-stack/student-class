1e16bb339efe154ce8453830053606a3a98286e8
Tuyen Ha <98144714+jane-stack@users.noreply.github.com>
2023-06-23 19:59:10 -0600

    updated loginpage.js

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,6 +1,29 @@
+import { useState } from "react";
+import LoginForm from "../auth/LoginForm";
+import SignupForm from "../auth/SignupForm";
+
+
 function LoginPage() {
+    const [ showLogin, setShowLogin ] = useState(true);
+
     return (
-        <div>Hello from LoginPage</div>
+        <div>
+            {showLogin ? (
+                <>
+                <LoginForm />
+                <p className='words'>Join ChatSpace? &nbsp;
+                    <button onClick={() => setShowLogin(false)}>Signup</button>
+                </p>
+                </>
+            ):(
+                <>
+                <SignupForm />
+                <p className='words'>Already a member? &nbsp;
+                    <button onClick={() => setShowLogin(true)}>Login</button>
+                </p>
+                </>
+            )}
+        </div>
     )
 }
 
