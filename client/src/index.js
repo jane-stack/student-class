c62ab211aa70f6d7e59dedcddc31d50256af110b
Tuyen Ha <98144714+jane-stack@users.noreply.github.com>
2023-06-23 19:20:14 -0600

    updated index.js to include providers

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ErrorsProvider } from './context/ErrorsContext'
+import { StudentProvider } from './context/StudentContext'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ErrorsProvider>
+      <StudentProvider>
+        <App />
+      </StudentProvider>
+    </ErrorsProvider>
   </React.StrictMode>
 );
 
